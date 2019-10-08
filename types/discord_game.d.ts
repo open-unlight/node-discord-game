8144c1a6e9d20849b5971896f56b7236821b6a6a
蒼時弦也 <elct9620@frost.tw>
2019-10-08 12:12:57 +0800

    Add TypeScript declaration

---
+++ types/discord_game.d.ts
@@ -0,0 +1,62 @@
+// Type definitions for Discord Game
+// Project: Open Unlight
+// Definitions by: Aotokitsuruya <https://blog.frost.tw>
+
+/**
+ * Discord Main Function
+ */
+export as namespace Discord;
+
+export const version: number;
+export function create(appId: string);
+export function runCallback(): boolean;
+
+/**
+ * Application
+ */
+
+export namespace Application {
+  export const locale: string;
+  export const branch: string;
+  export function getOAuth2Token(): Promise<string>;
+}
+
+/**
+ * Activity
+ */
+
+export namespace Activity {
+  export interface ActivityAssets {
+    largeImage?: string;
+    largeText?: string;
+    smallImage?: string;
+    smallText?: string
+  }
+
+  export interface ActivityTimestamps {
+    startAt?: Date;
+    endAt?: Date;
+  }
+
+  export interface ActivitySecrets {
+    match?: string;
+    join?: string;
+    spectate?: string;
+  }
+
+  export interface ActivityParty {
+    id?: string;
+    currentSize?: number;
+    maxSize?: number;
+  }
+
+  export interface Activity {
+    state?: string;
+    details?: string;
+    assets?: ActivityAssets;
+    timestamps?: ActivityTimestamps;
+    secrets?: ActivitySecrets;
+    party?: ActivityParty;
+  }
+  export function update(activity: Activity): Promise<boolean>;
+}
